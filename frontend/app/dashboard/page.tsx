85d94f9a0b7df8aa3b76d5452c67fed2c8a09a44
Ghada Chouichi <57904143+Ghada-Ch@users.noreply.github.com>
2026-04-12 15:37:13 +0100

    Update page.tsx

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default function DashboardPage() {
   
   useEffect(() => {
     async function fetchProjects() {
-      const res = await fetch(process.env.NEXT_PUBLIC_API_URL!, {
+      const res = await fetch("https://taski-665e.onrender.com/graphql", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
